c16c01eea5a0957be547b8d61118defa7399f7b2
Stan Viltouski <vyts_vyts@mail.ru>
2019-02-03 15:36:06 +0300

    StoStankov update

--- stostankov/wp-config.php
+++ stostankov/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'belpereplet1_stostankov');
+define('DB_NAME', 'stanki');
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'belpereplet1_s');
+define('DB_USER', 'root');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', '7n1FAukD');
+define('DB_PASSWORD', '');
 
 /** Имя сервера MySQL */
 define('DB_HOST', 'localhost');
